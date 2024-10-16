71334aa7521ccf5ee9bf555478f6d1b2c94d017e
Gege-alii <gegeeealinasr@gmail.com>
2024-10-16 19:28:28 +0300

    first commit

---
+++ src/app/weatherapi/weatherapi.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2'; 
+
+
+
+@Component({
+  selector: 'app-weatherapi',
+  templateUrl: './weatherapi.component.html',
+  styleUrls: ['./weatherapi.component.css']
+})
+export class WeatherapiComponent {
+
+
+  city: string = '';
+  weather: any;
+  forecast: any[] = [];
+  apiKey: string = 'dad5314993e34114b52162214241410';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    (window as any).AOS.init();
+  }
+
+  fetchWeather() {
+    if (this.city) {
+      this.http.get(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}`)
+        .subscribe(
+          (data: any) => {
+            this.weather = data;
+            this.fetchForecast(this.city);
+          },
+          (error) => {
+            this.weather = null;
+            this.forecast = [];
+            Swal.fire({
+              title: 'City not found!',
+              text: 'Please check the city name and try again.',
+              icon: 'error',
+              confirmButtonText: 'OK'
+            });
+            
+          }
+        );
+    }
+  }
+
+  
+
+  fetchForecast(city: string) {
+    this.http.get(`http://api.weatherapi.com/v1/forecast.json?key=${this.apiKey}&q=${city}&days=5`)
+      .subscribe(
+        (data: any) => {
+          this.forecast = data.forecast.forecastday;
+        },
+        (error) => {
+          alert('Error fetching forecast data');
+        }
+      );
+  }
+}
